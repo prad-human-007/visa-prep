b066243807845de0b319cc4958a670ab6ab79178
prad-human-007 <kanasepradhumn@gmail.com>
2024-10-20 12:13:45 +0530

    added prisma db

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,5 @@
 import { authConfig } from "@/lib/auth";
+import prisma from "@/lib/db";
 import { loginIsRequiredServer } from "@/lib/protectedRoute";
 import { getServerSession } from "next-auth";
 
@@ -6,12 +7,20 @@ import { getServerSession } from "next-auth";
 const page = async () => {
 
     const session = await getServerSession(authConfig);
+    const posts = await prisma.post.findMany();
 
     return(
         <div>
             <h1>This is the default post page</h1>
             <h1>This is the user name: {session?.user?.name}</h1>
-        </div>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <h1>{post.title}</h1>
+                    </li>
+                ))}
+            </ul>
+        </div> 
     )
 }
 
